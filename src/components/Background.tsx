55f10fbf9baa8c89779eb3c19b415cf0f59b4d21
geeky-auro <aurosaswat.apk@gmail.com>
2025-01-11 20:24:45 +0530

    Sign In Page Completed

---
+++ src/components/Background.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Background = () => {
+  return (
+    <div>
+      <img
+        src="https://cdn.pixabay.com/photo/2024/10/08/02/15/ai-generated-9104182_1280.jpg"
+        className="h-screen w-screen bg-cover bg-no-repeat"
+      />
+    </div>
+  );
+};
+
+export default Background;
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import Header from "./Header";
+import Background from "./Background";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="relative min-h-screen bg-gray-100">
+      <Header />
+      <div className="absolute inset-0 flex items-center justify-center">
+        <form className="w-3/12 bg-black opacity-90 p-12 rounded-lg shadow-lg">
+          <h1 className="text-3xl p-4 text-white font-bold">Sign In</h1>
+          <input
+            type="text"
+            placeholder="Email Address"
+            className="p-2 m-2 w-full border rounded-md bg-gray-700 text-white"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="p-2 m-2 w-full border rounded-md bg-gray-700 text-white"
+          />
+          <button
+            type="submit"
+            className="p-2 m-2 bg-red-500 text-white font-semibold rounded-full shadow-md hover:bg-red-700 focus:outline-none focus:ring focus:ring-red-400 focus:ring-opacity-75 w-full"
+          >
+            Sign In
+          </button>
+          <p className="p-2 text-white">
+            New to Netflix?{" "}
+            <span
+              className="text-red-700 font-bold hover:cursor-pointer"
+              onClick={() => {}}
+            >
+              Sign Up
+            </span>
+          </p>
+        </form>
+      </div>
+      <Background />
+    </div>
+  );
+};
 
-export default Login
+export default Login;
