9e44a145db6d8f1741af1ab952a1737f1f5e7bd6
geeky-auro <aurosaswat.apk@gmail.com>
2025-01-11 18:46:08 +0530

    Initial Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import "./App.css";
+import Body from "./components/Body";
 
 function App() {
   return (
     <>
-      <div className="flex-wrap align-middle">
-        <h1 className="text-3xl font-bold underline align-middle text-indigo-400">
-          Let's Build NetflixGPT
-        </h1>
-      </div>
+      <Body />
     </>
   );
 }
