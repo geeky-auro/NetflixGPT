5677486565bcc91e2a438828a86226a92986dba9
geeky-auro <aurosaswat.apk@gmail.com>
2025-01-11 17:32:16 +0530

    Setting up Environment for Building Netflix GPT

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex-wrap align-middle">
+        <h1 className="text-3xl font-bold underline align-middle text-indigo-400">
+          Let's Build NetflixGPT
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
