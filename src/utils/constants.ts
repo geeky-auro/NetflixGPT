a2d4595a7fb118336052c23c9e5245f995ab2693
geeky-auro <aurosaswat.apk@gmail.com>
2025-01-18 08:49:09 +0530

    API Fetching and TMDB Setup

---
+++ src/utils/constants.ts
@@ -0,0 +1,8 @@
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4Zjk0ZGI0MmJmNDMwNGRiY2U4ZDVlM2YyZGU5Y2QzOSIsIm5iZiI6MTczNzE2MjQwNi43MDU5OTk5LCJzdWIiOiI2NzhhZmVhNmFmY2IzMmM0OTk0MmVkZWIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.zQuIl3xVOrbKx2GXlwSm0jyrr7Xi6b4yay_m86pxnUg",
+  },
+};
