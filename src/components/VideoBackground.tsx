ccd1496f7bd61eb72a2eb55f31c2055ce15cfc0d
geeky-auro <aurosaswat.apk@gmail.com>
2025-01-20 00:46:44 +0530

    Storing Trailer Video in Redux Store

--- src/components/VideoBackground.tsx
+++ src/components/VideoBackground.tsx
@@ -1,7 +1,12 @@
 import React, { useEffect } from "react";
 import { API_OPTIONS } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addTrailerVideo } from "../utils/movieSlice";
 
 const VideoBackground = ({ movieId }) => {
+  const trailerVideo = useSelector((store: any) => store.movies?.movieTrailer);
+  const dispatch = useDispatch();
+
   // fetch trailer video
   const fetchTrailerVideo = async () => {
     const response = await fetch(
@@ -9,10 +14,11 @@ const VideoBackground = ({ movieId }) => {
       API_OPTIONS
     );
     const data = await response.json();
-    const trailer = data?.results?.filter(
+    const trailerFilter = data?.results?.filter(
       (video, index) => video.type === "Trailer"
     );
-    console.log(trailer[0]);
+    const trailer = trailerFilter?.length > 0 ? trailerFilter[0] : null;
+    dispatch(addTrailerVideo(trailer));
   };
 
   useEffect(() => {
@@ -23,7 +29,7 @@ const VideoBackground = ({ movieId }) => {
       <iframe
         width="1520"
         height="607"
-        src="https://www.youtube.com/embed/LH1J1EbqCaI?autoplay=1&loop=1&controls=0"
+        src={`https://www.youtube.com/embed/${trailerVideo?.key}?autoplay=1&loop=1&controls=0`}
         title="Sonic Trailer"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
